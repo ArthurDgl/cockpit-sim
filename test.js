74688dc4885b953ea3d3e0b07ff1101d64b45e54
Arthur <arthur.dugnolle@gmail.com>
2026-05-18 10:38:56 +0200

    msfs tests

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 const simConnect = require('node-simconnect');
+const SimConnectDataType = simConnect.SimConnectDataType;
+const SimConnectConstants = simConnect.SimConnectConstants;
+const SimConnectPeriod = simConnect.SimConnectPeriod;
 
 const EVENT_ID_PAUSE = 1;
+const REQUEST_1 = 0;
+const DEFINITION_1 = 0;
+
+let simData = {};
 
 simConnect.open('Cockpit Simulator', simConnect.Protocol.KittyHawk)
   .then(({recvOpen, handle}) => {
@@ -27,7 +34,36 @@ simConnect.open('Cockpit Simulator', simConnect.Protocol.KittyHawk)
     });
 
     handle.subscribeToSystemEvent(EVENT_ID_PAUSE, 'Pause');
+
+    handle.addToDataDefinition(DEFINITION_1, 'Kohlsman setting hg', 'inHg', SimConnectDataType.FLOAT64);
+    handle.addToDataDefinition(DEFINITION_1, 'Indicated Altitude', 'feet', SimConnectDataType.FLOAT64);
+    handle.addToDataDefinition(DEFINITION_1, 'Plane Latitude', 'degrees', SimConnectDataType.FLOAT64);
+    handle.addToDataDefinition(DEFINITION_1, 'Plane Longitude', 'degrees', SimConnectDataType.FLOAT64);
+    handle.addToDataDefinition(DEFINITION_1, 'VERTICAL SPEED', 'Feet per second', SimConnectDataType.INT32);
+
+    handle.requestDataOnSimObject(REQUEST_1, DEFINITION_1, SimConnectConstants.OBJECT_ID_USER, SimConnectPeriod.SIM_FRAME);
+
+    handle.on('simObjectData', recvSimObjectData => {
+        switch (recvSimObjectData.requestID) {
+            case REQUEST_1: {
+                const receivedData = {
+                    // Read order is important!
+                    kohlsmann: recvSimObjectData.data.readFloat64(),
+                    altitude: recvSimObjectData.data.readFloat64(),
+                    latitude: recvSimObjectData.data.readFloat64(),
+                    longitude: recvSimObjectData.data.readFloat64(),
+                    verticalSpeed: recvSimObjectData.data.readInt32(),
+                }
+                simData = receivedData;
+                break;
+            }
+        }
+    });
   })
   .catch(function (error) {
       console.log('Connection failed:', error);
   });
+
+setInterval(() => {
+    console.log(simData);
+}, 1000);
